d7dda98cb4349e50d8c2589ab54c504c0c70268a
Vaughn-Alston <vaughn2code@gmail.com>
2025-04-12 21:05:09 -0700

    Creating the Struct Node to traverse over the list

---
+++ Math_stack.cpp
@@ -0,0 +1,23 @@
+//Programer Vaughn
+//Version 1.0
+
+#include <iostream>
+using namespace std;
+
+class DynStack
+{
+    private:
+        struct Node
+        {
+            int value;
+            Node* next;
+        };
+          Node* top; // point to the top of the stack
+
+    public: ~DynStack();
+            
+
+
+
+
+};
